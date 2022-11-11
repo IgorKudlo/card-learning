32dbc4be0e32a2efca65917ade01ff3b2b67a930
igor.kudlo <igor.kudlo@spiralscout.com>
2022-11-11 22:05:17 +0300

    add axios, authAPI

---
+++ src/api/cards-api.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import {
+  IBlockResponse,
+  ILoginParams,
+  ILoginResponse,
+  ILogoutResponse,
+  IMeParams,
+  IMeResponse,
+  IRecoveryParams,
+  IRecoveryResponse,
+  IRegisterParams,
+  IRegisterResponse,
+  IResetParams,
+  IResetResponse
+} from './types';
+
+const settings = {
+  withCredentials: true,
+}
+export const instance = axios.create({
+  baseURL: 'http://localhost:7542/2.0/',
+  // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
+  // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
+  ...settings,
+})
+
+// api
+export const cardsAPI = {
+
+}
+
+export const authAPI = {
+  login(data: ILoginParams) {
+    return instance.post<ILoginResponse>('auth/login', data);
+  },
+  register(data: IRegisterParams) {
+    return instance.post<IRegisterResponse>('auth/register', data);
+  },
+  me(data: IMeParams) {
+    return instance.put<IMeResponse>('auth/me', data);
+  },
+  logout() {
+    return instance.delete<ILogoutResponse>('auth/me', {});
+  },
+  recovery(data: IRecoveryParams) {
+    return instance.post<IRecoveryResponse>('auth/forgot', data);
+  },
+  reset(data: IResetParams) {
+    return instance.post<IResetResponse>('auth/set-new-password', data);
+  },
+  block(data: any) {
+    return instance.post<IBlockResponse>('auth/block', data);
+  }
+}
---
+++ src/api/types.ts
@@ -0,0 +1,76 @@
+export interface ILoginParams {
+  email: string,
+  password: string,
+  rememberMe: boolean,
+}
+
+export interface ILoginResponse {
+  _id: string,
+  email: string,
+  name: string,
+  avatar?: string,
+  publicCardPacksCount: number,
+  created: Date,
+  updated: Date,
+  isAdmin: boolean,
+  verified: boolean,
+  rememberMe: boolean,
+  error?: string,
+}
+
+export interface IRegisterParams {
+  email: string,
+  password: string,
+}
+
+export interface IRegisterResponse {
+  addedUser: {},
+  error?: string,
+}
+
+export interface IMeParams {
+  name: string,
+  avatar: string,
+}
+
+export interface IMeResponse {
+  updatedUser: {},
+  error?: string,
+}
+
+export interface ILogoutResponse {
+  info: string,
+  error: string,
+}
+
+export interface IRecoveryParams {
+  email: string,
+  from: string,
+  message: string,
+}
+
+export interface IRecoveryResponse {
+  info: string,
+  error: string;
+}
+
+export interface IResetParams {
+  password: string,
+  resetPasswordToken: string,
+}
+
+export interface IResetResponse {
+  info: string,
+  error: string,
+}
+
+export interface IBlockParams {
+  id: string,
+  blockReason: string,
+}
+
+export interface IBlockResponse {
+  user: string,
+  blockedCardPacksCount: number,
+}
+
